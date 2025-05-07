b56a071ee9484c5d62f97f6a51bda0bea6c89a4b
Seymen <137383070+seymenay@users.noreply.github.com>
2025-05-07 17:47:27 +0300

    Edited  Readme, created imdb_movies, imported the imbdb dataset

---
+++ imdb_movies.sql
@@ -0,0 +1,33 @@
+-- Question 1: List all movie titles released in 2010
+
+-- Question 2: Find the birth year of Emma Stone
+
+-- Question 3: Movies released on or after 2016 (alphabetical)
+
+-- Question 4: Count of movies with a 10.0 rating
+
+-- Question 5: Titles and years of all Harry Potter films
+
+-- Question 6: Average rating of movies released in 2012
+
+-- Question 7: Movies from 2010 sorted by rating and title
+
+-- Question 8: Actors in Toy Story
+
+-- Question 9: Unique actors in 2004 films by birth year
+
+-- Question 10: Directors of movies rated 9.0 and above
+
+-- Question 11:Top 5 movies starring Chadwick Boseman
+
+-- Question 12: Movies starring both Bradley Cooper and J. Lawrence
+
+-- Question 13: Co-stars of Kevin Bacon (born 1958)
+
+-- Question 14: Most frequent actor in top 100 rated movies
+
+-- Question 15: Year with the highest number of movie releases
+
+-- Question 16: Average rating of each genre
+
+-- Question 17: Movies with missing ratings
